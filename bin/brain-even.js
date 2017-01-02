2101922ee37907ec5ad34b1c29ed4686857ceead
anton.azgarovich <anton.azgarovich@gmail.com>
2017-01-02 22:51:52 +0300

    fixed msg greeting

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -7,4 +7,4 @@ console.log('Welcome to the Brain Games!');
 
 const userName = readlineSync.question('May I have your name? ');
 
-console.log(`Hello ${userName}`);
+console.log(`Hello, ${userName}!`);
