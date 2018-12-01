ade95f5349d09b229961190d0486416f7148de80
anton.azgarovich <anton.azgarovich@gmail.com>
2018-12-02 00:41:22 +0300

    refactored generate list of quiz

--- core/createListOfQuiz.js
+++ core/createListOfQuiz.js
@@ -1,7 +1,8 @@
-export default (generator, quantity) => {
-  const arrayOfQuiz = [];
-  for (let i = 0; i < quantity; i++) {
-    arrayOfQuiz.push(generator());
-  }
-  return arrayOfQuiz;
-};
+const getValue = func => func();
+const createFilledArray = (size, func) => Array.from(Array(size), () => func());
+
+const createQuiz = ({ question, solution, answer = null }) => ({ question, solution, answer, isCorrect: answer !== solution });
+
+export const setAnswer = (quiz, answer) => createQuiz({ ...quiz, answer });
+
+export const createListOfQuiz = (generatorQuiz, attempts) => createFilledArray(attempts, () => createQuiz(getValue(generatorQuiz)));
--- core/mainLogic.js
+++ core/mainLogic.js
@@ -1,35 +1,35 @@
-import createListOfQuiz from './createListOfQuiz';
+import { setAnswer, createListOfQuiz } from './createListOfQuiz';
 
-export default ({ write, read }) => ({ description, generatorQuiz, attempts = 3 }) => {
-  const listOfQuiz = createListOfQuiz(generatorQuiz, attempts);
+export default ({ write, read }) =>
+  ({ description, generatorQuiz, attempts = 3 }) => {
+    write('Welcome to the Brain Games!\n');
+    write(`Quiz description: ${description}\n\n`);
 
-  write('Welcome to the Brain Games!\n');
-  write(`Quiz description: ${description}\n\n`);
+    write('May I have your name? ');
+    const playerName = read();
+    write(`Hello, ${playerName}!\n\n`);
 
-  write('May I have your name? ');
-  const playerName = read();
-  write(`Hello, ${playerName}!\n\n`);
+    createListOfQuiz(generatorQuiz, attempts).reduce((lastAnsweredCorrectly, quiz, index) => {
+      if (lastAnsweredCorrectly === false) {
+        return false;
+      }
 
-  listOfQuiz.reduce((lastAnsweredCorrectly, { question, solution }, index) => {
-    if (lastAnsweredCorrectly === false) {
-      return false;
-    }
+      write(`Question: ${quiz.question}\nYour answer: `);
+      const playerAnswer = read();
+      const quizWithAnswer = setAnswer(quiz, playerAnswer);
 
-    write(`Question: ${question}\nYour answer: `);
-    const playerResponse = read();
+      if (quizWithAnswer.isCorrect) {
+        write(`\n"${quizWithAnswer.answer}" is wrong answer ;(. Correct answer was "${quizWithAnswer.solution}".\n`);
+        write(`Let's try again, ${playerName}!\n`);
+        return false;
+      }
 
-    if (playerResponse !== solution) {
-      write(`\n"${playerResponse}" is wrong answer ;(. Correct answer was "${solution}".\n`);
-      write(`Let's try again, ${playerName}!`);
-      return false;
-    }
+      write('Correct!\n\n');
 
-    write('Correct!\n\n');
-
-    const attempt = index + 1;
-    if (attempt === attempts) {
-      write(`Congratulations, ${playerName}!\n`);
-    }
-    return true;
-  }, true);
-};
+      const attempt = index + 1;
+      if (attempt === attempts) {
+        write(`Congratulations, ${playerName}!\n`);
+      }
+      return true;
+    }, true);
+  };
