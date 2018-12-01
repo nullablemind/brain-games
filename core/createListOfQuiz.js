5ad58c1f1d4fea85c6bc666f6092e972029ddad1
anton.azgarovich <anton.azgarovich@gmail.com>
2018-12-02 00:47:07 +0300

    remove unused structure

--- core/createListOfQuiz.js
+++ core/createListOfQuiz.js
@@ -1,7 +1,7 @@
 const getValue = func => func();
 const createFilledArray = (size, func) => Array.from(Array(size), () => func());
 
-const createQuiz = ({ question, solution, answer = null }) => ({ question, solution, answer, isCorrect: answer !== solution });
+const createQuiz = ({ question, solution }) => ({ question, solution });
 
 export const isRightAnswer = (quiz, answer) => quiz.solution === answer;
 
