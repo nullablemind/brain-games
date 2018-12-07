862cabe26b9b5a82ce8db98416d776c7f77dd86f
anton.azgarovich <anton.azgarovich@gmail.com>
2018-12-08 02:02:37 +0300

    rename param

--- core/createListOfQuiz.js
+++ core/createListOfQuiz.js
@@ -1,5 +1,5 @@
 const createFilledArray = (size, func) => Array.from(Array(size), () => func());
 
-export const isRightAnswer = (quiz, answer) => quiz.solution === answer;
+export const isRightAnswer = (problem, answer) => problem.solution === answer;
 
 export const generateProblems = ({ generatorQuiz, attempts = 3 }) => createFilledArray(attempts, generatorQuiz);
