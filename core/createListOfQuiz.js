49f696379cba6195c11f8d2ff2a5ed35f18e47c3
anton.azgarovich <anton.azgarovich@gmail.com>
2018-12-08 01:14:18 +0300

    remove unused function

--- core/createListOfQuiz.js
+++ core/createListOfQuiz.js
@@ -1,8 +1,5 @@
-const getValue = func => func();
 const createFilledArray = (size, func) => Array.from(Array(size), () => func());
-
-const createQuiz = ({ question, solution }) => ({ question, solution });
 
 export const isRightAnswer = (quiz, answer) => quiz.solution === answer;
 
-export const createListOfQuiz = (generatorQuiz, attempts) => createFilledArray(attempts, () => createQuiz(getValue(generatorQuiz)));
+export const createListOfQuiz = (generatorQuiz, attempts) => createFilledArray(attempts, generatorQuiz);
