b4d72566fa35d9d470afa4d4022c7924d4fb2cd6
anton.azgarovich <anton.azgarovich@gmail.com>
2018-12-08 20:05:55 +0300

    fixed max 100 size line string

--- core/lib.js
+++ core/lib.js
@@ -2,4 +2,5 @@ const createFilledArray = (size, func) => Array.from(Array(size), () => func());
 
 export const isEquals = (value1, value2) => value1 === value2;
 
-export const generateProblems = ({ generatorProblem, attempts = 3 }) => createFilledArray(attempts, generatorProblem);
+export const generateProblems = ({ generatorProblem, attempts = 3 }) =>
+  createFilledArray(attempts, generatorProblem);
