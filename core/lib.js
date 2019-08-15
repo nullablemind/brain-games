30170f466b902ea5decfea6cb6dfba3b14402796
anton.azgarovich <anton.azgarovich@gmail.com>
2019-08-15 11:47:27 +0300

    remove all release

--- core/lib.js
+++
@@ -1,6 +0,0 @@
-const createFilledArray = (size, func) => Array.from(Array(size), () => func());
-
-export const isEquals = (value1, value2) => value1 === value2;
-
-export const generateProblems = ({ generatorProblem, attempts = 3 }) =>
-  createFilledArray(attempts, generatorProblem);
