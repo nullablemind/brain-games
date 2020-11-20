4a22f00c55d6c6e71f0668737a7c3852e1b11f18
anton.azgarovich <anton.azgarovich@gmail.com>
2020-11-21 00:11:52 +0500

    Refactoring test lost case

--- test/index.js
+++ test/index.js
@@ -3,16 +3,16 @@ const game = require('../src');
 
 const speakLog = (text) => ({ method: 'speak', text });
 const askLog = (question) => ({ method: 'ask', question });
+const problem = (number) => ({ description: `problem desc ${number}`, solution: `prob solution ${number}` });
+const catridge = {
+  gameDescription: 'game desc',
+  generateProblem: problem,
+};
+const playerName = 'Mike';
+
 
 describe('game() - won case', async (assert) => {
   const log = [];
-
-  const problem = (number) => ({ description: `problem desc ${number}`, solution: `prob solution ${number}` });
-  const playerName = 'Mike';
-  const catridge = {
-    gameDescription: 'game desc',
-    generateProblem: problem,
-  };
 
   const io = {
     speak: (text) => log.push({ method: 'speak', text }),
@@ -56,13 +56,8 @@ describe('game() - won case', async (assert) => {
 describe('game() - lost case', async (assert) => {
   const log = [];
 
-  const problem = { description: 'prob desc', solution: 'prob solution' };
-  const playerName = 'Mike';
   const wrongPlayerAnswer = 'wrong answer';
-  const catridge = {
-    gameDescription: 'game desc',
-    generateProblem: () => problem,
-  };
+
   const speak = (text) => log.push({ method: 'speak', text });
   const ask = (question, problemNumber) => {
     log.push({ method: 'ask', question });
@@ -72,27 +67,31 @@ describe('game() - lost case', async (assert) => {
     if (problemNumber === 2) {
       return wrongPlayerAnswer;
     }
-    return problem.solution;
+    return problem(problemNumber).solution;
   };
   game(catridge, { speak, ask });
 
   const expected = [
-    { method: 'speak', text: 'Welcome to the Brain Games!\n' },
-    { method: 'speak', text: `${catridge.gameDescription}\n\n` },
-    { method: 'ask', question: 'May I have your name, please? ' },
-    { method: 'speak', text: `Hello, ${playerName}!\n\n` },
-    { method: 'speak', text: `Question: ${problem.description}\n` },
-    { method: 'ask', question: 'Your answer: ' },
-    { method: 'speak', text: 'Correct!\n\n' },
-    { method: 'speak', text: `Question: ${problem.description}\n` },
-    { method: 'ask', question: 'Your answer: ' },
-    { method: 'speak', text: `"${wrongPlayerAnswer}" is wrong answer ;(. Correct answer was "${problem.solution}".\n\n` },
-    { method: 'speak', text: `Let's try again, ${playerName}!\n` },
+    speakLog('Welcome to the Brain Games!\n'),
+    speakLog(`${catridge.gameDescription}\n\n`),
+
+    askLog('May I have your name, please? '),
+    speakLog(`Hello, ${playerName}!\n\n`),
+
+    speakLog(`Question: ${problem(1).description}\n`),
+    askLog('Your answer: '),
+    speakLog('Correct!\n\n'),
+
+    speakLog(`Question: ${problem(2).description}\n`),
+    askLog('Your answer: '),
+
+    speakLog(`"${wrongPlayerAnswer}" is wrong answer ;(. Correct answer was "${problem(2).solution}".\n\n`),
+    speakLog(`Let's try again, ${playerName}!\n`),
   ];
 
   assert({
-    given: 'game()',
-    should: 'be right ordered methods call',
+    given: 'catridge and io',
+    should: 'call methods in correct order',
     actual: log,
     expected,
   });
